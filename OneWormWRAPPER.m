afef731de770f3184f5dc152e76f545c436bd391
mlab <mlab@GEN-JDZ3WH1.pu.win.princeton.edu>
2012-06-30 14:07:39 -0400

    debugging

--- OneWormWRAPPER.m
+++ OneWormWRAPPER.m
@@ -18,14 +18,14 @@
 %Documents\Dropbox\WormMovement\AdulanteSwimming\SwimData\fld2RESULTS\TestDS1_12_12test2\PIC_CL2120 A1 11RUNfinal'
 %InputDir='/Users/budoday/Dropbox/WormMovement/AdulanteSwimming/SwimData/fld2RESULTS/TestDS1_12_12test2/'
 %InputDir='/Users/budoday/Desktop/SwimData/fld1';
-InputDir='\\murphylab.princeton.edu\data\shared\CL2122_Adult_24h_March_6\Control_RNAi_CL2122';
+InputDir='\\murphylab.princeton.edu\data\shared\CL2122_Adult_24h_March_6\Control_RNAi_CL2122-2'
 Outputdir=InputDir;
-trialName='6_1_12Analysis';
+trialName='6_28_12Analysis';
 GetWorm(InputDir, Outputdir, trialName);
 %%
-InputDir=['\\murphylab.princeton.edu\data\shared\CL2122_Adult_24h_March_6\Control_RNAi_CL2122RESULTS' trialName];
+InputDir=[InputDir filesep trialName];
 Outputdir=InputDir;
-%ProcessSpine(InputDir, Outputdir, trialName);
+ProcessSpine(InputDir, Outputdir, trialName);
 %OneWormFigs(InputDir, Outputdir, trialName);
 %FancyFIGs
 %OneWormAnalysis
