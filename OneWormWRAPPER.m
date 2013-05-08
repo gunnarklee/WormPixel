08b4c9c204eac3528cc222752fa345a591ef4c76
Gunnar Kleemann <gunnarkl@gmail.com>
2013-05-08 08:23:57 -0400

    Merged

--- OneWormWRAPPER.m
+++ OneWormWRAPPER.m
@@ -1,11 +1,23 @@
 %% WORKFLOW for OneWorm.
 % specify your path to the input directory. 
-
 %InputDir='C:\Documents and Settings\mlab\Desktop\reproductive_span_robot\RSbatch2screen1-verify12_2011'
 %..InputDir='/Users/budoday/Dropbox/Movement/Movement_Analysis/REDO'
 
 % On a PC always specify a trialName
-trialName='REDOS';
+
+%InputDir='C:\Documents and Settings\mlab\My Documents\Dropbox\WormMovement\AdulanteSwimming\SwimData\fld2'
+
+%Documents\Dropbox\WormMovement\AdulanteSwimming\SwimData\fld2RESULTS\TestDS1_12_12test2\PIC_CL2120 A1 11RUNfinal'
+%InputDir='/Users/budoday/Dropbox/WormMovement/AdulanteSwimming/SwimData/fld2RESULTS/TestDS1_12_12test2/'
+%InputDir='/Users/budoday/Desktop/SwimData/fld1';
+
+%InputDir='\\murphylab.princeton.edu\data\shared\CL2120_Adult_24h_March_6\with_RNAi';
+%'\\murphylab.princeton.edu\data\shared\CL2120_Adult_24h_March_6\with_RNAi'
+%pinceton.edu\data\shared\CL2120_Adult_24h_March_6\control_RNAi'
+%InputDir='\\murphylab.princeton.edu\data\shared\CL2122_Adult_24h_March_6\with_RNAi_CL2122'
+%%InputDir='C:\Documents and Settings\mlab\My Documents\Dropbox\Analysis_Movement'
+%Outputdir=InputDir
+%trialName='7_4_12Run'
 
 [InputDir]=GetWorm()%(InputDir); % 'trialName', trialName
 %[InputDir]=ProcessSpine
